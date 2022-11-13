f963c6773305138b5df604db9471f88039f56c09
fboyle2001 <finlayboyle2001@gmail.com>
2022-11-13 21:53:05 +0000

    Runs, doesn't work well

--- Code-Rewrite/algorithms/hindsight_anchor.py
+++ Code-Rewrite/algorithms/hindsight_anchor.py
@@ -59,6 +59,12 @@ class HindsightAnchor(BaseCLAlgorithm):
         self.max_memory_per_class = 5
         self.fifo_buffer = buffers.FIFORingReplayBuffer(max_memory_per_class=self.max_memory_per_class)
         self.finetuning_epochs = 50
+        self.feature_embedding_moving_beta = 0.9 # Beta
+        self.regularisation_strength = 1.0 # This is lambda
+        self.gamma = 0.1 # No idea for the correct value
+        self.anchor_epochs = 100
+
+        self.mean_feature_embeddings = None
 
     @staticmethod
     def get_algorithm_folder() -> str:
@@ -155,7 +161,7 @@ class HindsightAnchor(BaseCLAlgorithm):
 
                 # Clip gradients
                 if self.gradient_clip is not None:
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.gradient_clip) # type: ignore
+                    torch.nn.utils.clip_grad_norm_(finetuned_model.parameters(), self.gradient_clip) # type: ignore
                 
                 finetuned_model_opt.step()
                 finetuning_running_loss += finetuned_loss.item()
@@ -166,13 +172,68 @@ class HindsightAnchor(BaseCLAlgorithm):
         
         return finetuned_model
 
+    def _update_mean_embeddings(self, B, B_targets): 
+        # Update the mean embedding
+        frozen_feature_extractor = copy.deepcopy(self.model)
+        frozen_feature_extractor.final = torch.nn.Identity()
+
+        # Experiment with transformed vs untransformed
+        for transformed_sample, target in zip(B, B_targets):
+            target = target.detach().cpu().item()
+            sample_features = frozen_feature_extractor(transformed_sample.unsqueeze(0).to(self.device)).squeeze(0).to(self.device)
+            updated_mean_feature_embedding = (self.feature_embedding_moving_beta * self.mean_feature_embeddings[target] + (1 - self.feature_embedding_moving_beta) * sample_features) # type: ignore
+            self.mean_feature_embeddings[target] = updated_mean_feature_embedding.detach() # type: ignore - must detach
+
+    def _future_model_update(self, A, A_targets):
+        # Copy the copy for a temporary update first
+        future_model = copy.deepcopy(self.model).to(self.device)
+        future_model_opt = optim.SGD(future_model.parameters(), lr=1e-3)
+
+        future_model_opt.zero_grad()
+        future_predictions = future_model(A)
+        future_loss = self.loss_criterion(future_predictions, A_targets)
+        future_loss.backward()
+        future_model_opt.step()
+
+        return future_model
+
+    def _confirm_new_anchors(
+        self,
+        current_task_anchors: torch.Tensor, 
+        anchor_data: Optional[torch.Tensor], 
+        anchor_classes: Optional[torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Save the new anchors
+        new_anchors = []
+        new_anchor_classes = []
+
+        for new_anchor_class, new_anchor in enumerate(current_task_anchors):
+            # Skip those that are all zero i.e. classes that never appeared
+            if torch.all(torch.eq(new_anchor, torch.zeros_like(new_anchor))):
+                continue
+                
+            new_anchors.append(new_anchor)
+            new_anchor_classes.append(torch.LongTensor([new_anchor_class]))
+
+        new_anchors = torch.stack(new_anchors).to(self.device)
+        new_anchor_classes = torch.stack(new_anchor_classes).to(self.device)
+
+        if anchor_data is None:
+            assert anchor_classes is None
+            return new_anchors.to(self.device), new_anchor_classes.long().to(self.device)
+        else:
+            assert anchor_classes is not None
+            return torch.cat([anchor_data, new_anchors], dim=0).to(self.device), torch.cat([anchor_classes, new_anchor_classes], dim=0).long().to(self.device)
+
     def train(self) -> None:
         super().train()
 
         ### START OF PARAMETERS 
         # batch_size = 16 (see self.batch_size)
-        episodic_memory_size = 5 * 5 * 10 # mem_size * num_tasks * total_classes
-        feature_embedding_moving_beta = 0.9
+        # episodic_memory_size = 5 * 5 * 10 # mem_size * num_tasks * total_classes
+        # feature_embedding_moving_beta = 0.9 # (see self.feature_embedding_moving_beta)
+        # lambda = 1.0
+        # gamma = ? 
         ### END OF PARAMETERS
 
         # Get the raw, untransformed data and their targets
@@ -183,10 +244,20 @@ class HindsightAnchor(BaseCLAlgorithm):
             } for task_no in range(len(self.dataset.task_datasets))
         }
 
+        anchor_data = None
+        anchor_classes = None
+
+        current_task_anchors = None
+
         for task_no in range(self.dataset.task_count):
+            current_task_anchors = torch.zeros([10, 3, 32, 32])
+
+            if anchor_data is not None:
+                assert anchor_classes is not None
+                logger.debug(f"Anchor shape (d/t): {anchor_data.shape}, {anchor_classes.shape}")
+
             # Store the mean feature embedding for each class
-            mean_feature_embeddings = torch.zeros([10, 512]).to(self.device)
-            forced_real_feature_embeddings = {}
+            self.mean_feature_embeddings = torch.zeros([10, 512]).to(self.device)
 
             logger.info(f"Task {task_no + 1} / {self.dataset.task_count}")
             logger.info(f"Classes in task: {self.dataset.resolve_class_indexes(self.dataset.task_splits[task_no])}")
@@ -204,13 +275,14 @@ class HindsightAnchor(BaseCLAlgorithm):
             )
 
             running_loss = 0
+            running_anchor_loss = 0
 
             for batch_no, (B_untransformed, B_targets) in enumerate(untransformed_task_dataloader):
                 # Preprocess the data into B, B_M, and A. Put all on the correct device.
                 B_untransformed, B_targets, B_M_untransformed, B_M_targets, A_untransformed, A_targets, B, B_M, A = self._preprocess_data(B_untransformed, B_targets)
 
-                # There are no anchors to train on for the first task!
                 if task_no == 0: 
+                    # There are no anchors to train on for the first task!
                     self.optimiser.zero_grad()
                     predictions = self.model(A)
                     loss = self.loss_criterion(predictions, A_targets) # / A.shape[0]
@@ -227,45 +299,95 @@ class HindsightAnchor(BaseCLAlgorithm):
                         logger.debug(f"Batch {batch_no}, RL: {running_loss / 40}")
                         running_loss = 0
                 else:
-                    sys.exit(0)
+                    # Now use the anchors as well
+                    future_model = self._future_model_update(A, A_targets)
+                    overall_loss = 0
 
-                # Update the mean embedding
-                frozen_feature_extractor = copy.deepcopy(self.model)
-                frozen_feature_extractor.final = torch.nn.Identity()
+                    self.optimiser.zero_grad()
+                    traditional_predictions = self.model(A)
+                    traditional_loss = self.loss_criterion(traditional_predictions, A_targets) # / A.shape[0]
+                    overall_loss += traditional_loss
 
-                # Experiment with transformed vs untransformed
-                for transformed_sample, target in zip(B, B_targets):
-                    target = target.detach().cpu().item()
-                    sample_features = frozen_feature_extractor(transformed_sample.unsqueeze(0).to(self.device)).squeeze(0).to(self.device)
+                    future_anchor_predictions = future_model(anchor_data)
+                    current_anchor_predictions = self.model(anchor_data)
 
-                    ## TEMP
-                    if target not in forced_real_feature_embeddings.keys():
-                        forced_real_feature_embeddings[target] = []
+                    anchor_loss = (current_anchor_predictions - future_anchor_predictions).square().mean(dim=1).sum()
+                    running_anchor_loss += anchor_loss.item()
+                    overall_loss += self.regularisation_strength * anchor_loss
+
+                    overall_loss.backward()
                     
-                    forced_real_feature_embeddings[target].append(sample_features.detach().cpu())
-                    ## END TEMP
+                    # Clip gradients
+                    if self.gradient_clip is not None:
+                        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.gradient_clip) # type: ignore
 
-                    mean_feature_embeddings[target] = (feature_embedding_moving_beta * mean_feature_embeddings[target] + (1 - feature_embedding_moving_beta) * sample_features).detach() #type: ignore
+                    self.optimiser.step()
+                    running_loss += overall_loss.item()
+
+                    if batch_no % 40 == 0 and batch_no != 0:
+                        logger.debug(f"Batch {batch_no}, RL: {running_loss / 40}, AL: {running_anchor_loss / 40}")
+                        running_loss = 0
+                        running_anchor_loss = 0
+
+                # Update the mean embeddings
+                self._update_mean_embeddings(B, B_targets)
 
                 # Update the episodic memory FIFO buffer
                 self._update_fifo_buffer(B_untransformed, B_targets)
-            
-            # Look at the mean feature extractions
-            for target in range(mean_feature_embeddings.shape[0]):
-                eval_mean = mean_feature_embeddings[target].mean()
-                loss_to_nought = eval_mean.square()
-                
-                if target in forced_real_feature_embeddings.keys():
-                    real_mean = torch.stack(forced_real_feature_embeddings[target])
-                    logger.debug(f"Real shape {real_mean.shape}")
-                    real_mean = real_mean.mean()
-                    diff = (real_mean - mean_feature_embeddings[target]).square().mean()
-                    logger.info(f"Target {target}: EM: {eval_mean}, L2N: {loss_to_nought}, RM: {real_mean}, D: {diff}")
-                else:
-                    logger.info(f"Target* {target}: EM: {eval_mean}, L2N: {loss_to_nought}")
 
+                # Populate the unfilled anchors
+                for sample, target in zip(B, B_targets): # B or B_untransformed?
+                    current_task_anchors[target] = sample #.permute(2, 0, 1)
+            
+            # Save the newly selected anchors
+            anchor_data, anchor_classes = self._confirm_new_anchors(current_task_anchors, anchor_data, anchor_classes)
+
+            ## ANCHOR TRAINING
             # Once the model has been updated, we finetune on the episodic memory
             finetuned_model = self._finetune_model()
+            anchor_parameters = nn.ParameterList([nn.parameter.Parameter(anchor) for anchor in anchor_data])
+            anchor_opt = optim.SGD(anchor_parameters, lr=1e-3)
+
+            frozen_feature_extractor = copy.deepcopy(self.model)
+            frozen_feature_extractor.final = torch.nn.Identity()
+
+            anchor_running_loss = 0
+
+            for anchor_epoch in range(self.anchor_epochs):
+                anchor_opt.zero_grad()
+
+                overall_loss = 0
+                anchor_finetuned_predictions = finetuned_model(anchor_data)
+                anchor_finetuned_loss = self.loss_criterion(anchor_finetuned_predictions, anchor_classes.squeeze())
+                overall_loss += anchor_finetuned_loss
+
+                anchor_actual_predictions = self.model(anchor_data)
+                anchor_actual_loss = self.loss_criterion(anchor_actual_predictions, anchor_classes.squeeze())
+                overall_loss += anchor_actual_loss
+
+                anchor_features = frozen_feature_extractor(anchor_data)
+                feature_loss = 0
+
+                for idx, anchor_feature in enumerate(anchor_features):
+                    individual_feature_loss = anchor_feature - self.mean_feature_embeddings[anchor_classes[idx]]
+                    individual_feature_loss = individual_feature_loss.mean().sum()
+                    feature_loss += individual_feature_loss
+                
+                overall_loss += feature_loss
+                overall_loss = -overall_loss # Want gradient ascent not descent
+                overall_loss.backward()
+                
+                # Clip gradients
+                if self.gradient_clip is not None:
+                    torch.nn.utils.clip_grad_norm_(anchor_parameters, self.gradient_clip) # type: ignore
+
+                anchor_opt.step()
+                anchor_running_loss += overall_loss
+
+                if anchor_epoch % 10 == 0 and anchor_epoch != 0:
+                    logger.debug(f"Batch {anchor_epoch}, RL: {anchor_running_loss / 10}")
+                    anchor_running_loss = 0
+                    logger.debug(f"Reset: {anchor_running_loss}")
 
             self.model.eval()
             self.run_base_task_metrics(task_no=task_no)
