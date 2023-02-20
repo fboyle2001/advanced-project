d2bfb0e4e2c0e2f4827415d7dd967ec44c9a4b00
fboyle2001 <finlayboyle2001@gmail.com>
2023-02-20 19:32:29 +0000

    Lots of results from the weekend

--- Code/multi.bat
+++ Code/multi.bat
@@ -1,9 +1,9 @@
 @echo off
 
 @REM set algorithm=scr
-set dataset=cifar10
-set cpt=2
-set samples=500
+set dataset=cifar100
+set cpt=20
+set samples=5000
 
 echo Configuration
 @REM echo Algorithm: %algorithm%
@@ -13,26 +13,8 @@ echo:
 
 @REM 9767, 2843, 2214, 5953, 2461
 
-FOR %%s IN (2843) DO (
-    FOR %%a in (novel_bn, novel_rd) DO (
-        echo Seed: %%s
-        echo Executing: python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
-        python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
-        echo:
-    )
-)
-
-FOR %%s IN (2214) DO (
-    FOR %%a in (ewc, gdumb, l2p, rainbow, scr, novel_bn, novel_rd) DO (
-        echo Seed: %%s
-        echo Executing: python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
-        python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
-        echo:
-    )
-)
-
-FOR %%s IN (5953, 2461) DO (
-    FOR %%a IN (der, derpp, ewc, gdumb, l2p, rainbow, scr, novel_bn, novel_rd) DO (
+FOR %%s IN (2843, 2214, 5953, 2461) DO (
+    FOR %%a in (novel_rd, novel_bn) DO (
         echo Seed: %%s
         echo Executing: python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
         python result_generator.py --algorithm %%a --dataset %dataset% --cpt %cpt% --seed %%s --samples %samples%
